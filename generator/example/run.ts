2d216b9d46d1b314b7c54c9c54445fa19604ce62
Ahmed K. A <ahmadkabdullah@pm.me>
2023-05-28 17:30:40 +0300

    Remove custom parser and use CheerioJS

--- generator/example/run.ts
+++
@@ -1,6 +0,0 @@
-import path from "path";
-import { parseTemplateFromFile } from "../parser";
-
-const temp = parseTemplateFromFile("test", path.join(__dirname, "./cv.html"));
-
-console.log(JSON.stringify(temp, null, 2));
