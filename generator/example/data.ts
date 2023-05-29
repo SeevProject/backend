8478b017a8104dbf952309582793e52e18c7b6d4
Ahmed K. A <ahmadkabdullah@pm.me>
2023-05-29 12:55:59 +0300

    Add checks for type and length of element and return problems in insertData

--- generator/example/data.ts
+++ generator/example/data.ts
@@ -1,76 +1,76 @@
 export const userData = {
-		name: {
-			first: "John",
-			middle: "Doe",
-			last: "Smith",
+	name: {
+		first: "John",
+		middle: "Doe",
+		last: "Smith",
+	},
+	phone: "23456789",
+	email: "john.doe@example.com",
+	picture: "https://example.com/profile_picture.jpg",
+	about: "I am a passionate and experienced software developer.",
+	status: {
+		employed: true,
+		student: false,
+	},
+	address: {
+		country: "United States",
+		city: "New York",
+		street: "123 Main Street",
+	},
+	languages: [
+		{
+			name: "English",
+			level: "Advanced",
 		},
-		phone: "1234567890",
-		email: "john.doe@example.com",
-		picture: "https://example.com/profile_picture.jpg",
-		about: "I am a passionate and experienced software developer.",
-		status: {
-			employed: true,
-			student: false,
+		{
+			name: "Spanish",
+			level: "Intermediate",
 		},
-		address: {
-			country: "United States",
-			city: "New York",
-			street: "123 Main Street",
+	],
+	education: [
+		{
+			facility: "University of ABC",
+			location: "New York",
+			degree_level: "Bachelor's",
+			degree_field: "Computer Science",
+			date_start: "2015-09-01",
+			date_end: "2019-05-31",
 		},
-		languages: [
-			{
-				name: "English",
-				level: "Advanced",
-			},
-			{
-				name: "Spanish",
-				level: "Intermediate",
-			},
-		],
-		education: [
-			{
-				facility: "University of ABC",
-				location: "New York",
-				degree_level: "Bachelor's",
-				degree_field: "Computer Science",
-				date_start: "2015-09-01",
-				date_end: "2019-05-31",
-			},
-		],
-		courses: [
-			{
-				title: "Web Development",
-				about: "Learn to build modern web applications.",
-				subjects: ["HTML", "CSS", "JavaScript"],
-				location: "Online",
-			},
-			{
-				title: "Database Management",
-				about: "Master the skills to manage databases effectively.",
-				subjects: ["SQL", "Database Design"],
-				location: "University of XYZ",
-			},
-		],
-		skills: [
-			{
-				name: "JavaScript",
-				about: "Proficient in writing JavaScript code.",
-				level: 4,
-			},
-			{
-				name: "React",
-				about: "Experienced in building user interfaces with React.",
-				level: 3,
-			},
-		],
-		hobbies: [
-			{
-				name: "Photography",
-				about: "Capture moments and tell stories through photographs.",
-			},
-			{
-				name: "Hiking",
-				about: "Explore nature and enjoy outdoor adventures.",
-			},
-		],
+	],
+	courses: [
+		{
+			title: "Web Development",
+			about: "Learn to build modern web applications.",
+			subjects: ["HTML", "CSS", "JavaScript"],
+			location: "Online",
+		},
+		{
+			title: "Database Management",
+			about: "Master the skills to manage databases effectively.",
+			subjects: ["SQL", "Database Design"],
+			location: "University of XYZ",
+		},
+	],
+	skills: [
+		{
+			name: "JavaScript",
+			about: "Proficient in writing JavaScript code.",
+			level: 4,
+		},
+		{
+			name: "React",
+			about: "Experienced in building user interfaces with React.",
+			level: 3,
+		},
+	],
+	hobbies: [
+		{
+			name: "Photography",
+			about: "Capture moments and tell stories through photographs.",
+		},
+		{
+			name: "Hiking",
+			about: "Explore nature and enjoy outdoor adventures.",
+		},
+	],
 };
--- generator/replacer.ts
+++ generator/replacer.ts
@@ -1,4 +1,5 @@
 import { CheerioAPI } from "cheerio";
+import { log } from "console";
 
 export function handleRepeats(doc: CheerioAPI) {
 	// go through all elements with repeat attribute
@@ -40,13 +41,60 @@ export function handleRepeats(doc: CheerioAPI) {
 }
 
 export function insertData(doc: CheerioAPI, data: any) {
+	let problems: string[] = [];
+
 	doc("[id]").each((_, element) => {
+		// get element id
 		const elementId = doc(element).attr("id");
 		if (!elementId) return;
 
-		const elementValue = data[elementId];
-		if (!elementValue) return;
+		// get the value to insert at the id
+		let dataValue = data[elementId];
+		if (!dataValue) return;
 
-		doc(element).text(elementValue);
+		// compare lengths
+
+		const elementLength = doc(element).attr("length");
+		if (elementLength) {
+			const elementLengthValue = parseInt(elementLength);
+
+			if (dataValue.length > elementLengthValue) {
+				problems.push(
+					`${dataValue} is ${dataValue.length} characters long. But no more than ${elementLength} is accepted on ${elementId}`,
+				);
+				dataValue = dataValue.substring(0, elementLengthValue);
+			}
+		}
+
+		// compare type
+
+		const elementType = doc(element).attr("type");
+
+		if (elementType) {
+			if (elementType == "string" && typeof dataValue !== "string") {
+				problems.push(
+					`${dataValue} is not a string. But a string is expected on ${elementId}`,
+				);
+			} else if (elementType == "number") {
+				const dataValueNumber = parseInt(dataValue);
+				if (isNaN(dataValueNumber)) {
+					problems.push(
+						`${dataValue} is not a number. But a number is expected on ${elementId}`,
+					);
+				} else {
+					dataValue = dataValueNumber;
+				}
+			}
+		}
+
+		// insert the data
+
+		log(`Inserting ${dataValue} into ${elementId}`);
+
+		doc(element).text(dataValue);
 	});
+
+	console.log(problems);
+
+	return problems;
 }
