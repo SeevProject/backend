5a2978646bdfd27c889304d893f53e332302ca0b
lanya yousif <lanyayusf541@gmail.com>
2023-05-30 16:39:15 +0300

    add optional to company validation

--- validation/company.validation.ts
+++ validation/company.validation.ts
@@ -4,36 +4,36 @@ export const CompanyValidation = (body) =>{
 const validation = z.object({
 	permissions: z.array(
 		z.object({
-			contact: z.boolean(),
+			contact: z.boolean().optional(),
 			status: z.object({
-				employed: z.boolean(),
-				student: z.boolean(),
-			}),
+				employed: z.boolean().optional(),
+				student: z.boolean().optional(),
+			}).optional(),
 			languages: z.array(
 				z.object({
-					name: z.string(),
-				}),
-			),
+					name: z.string().optional(),
+				}).optional(),
+			).optional(),
 			skills: z.array(
 				z.object({
-					name: z.string(),
-					level: z.number(),
-				}),
-			),
+					name: z.string().optional(),
+					level: z.number().optional(),
+				}).optional(),
+			).optional(),
 			courses: z.array(
 				z.object({
-					subjects: z.string(),
-				}),
-			),
+					subjects: z.string().optional(),
+				}).optional(),
+			).optional(),
 			education: z.array(
 				z.object({
-					facility: z.string(),
-					degree_level: z.string(),
-					degree_field: z.string(),
-				}),
-			),
-		}),
-	),
+					facility: z.string().optional(),
+					degree_level: z.string().optional(),
+					degree_field: z.string().optional(),
+				}).optional(),
+			).optional(),
+		}).optional(),
+	).optional(),
 }).required();
 
 const result = validation.safeParse(body);
