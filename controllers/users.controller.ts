384ae4cabeb47cf936239264fe35ec891f805dc5
lanya yousif <lanyayusf541@gmail.com>
2023-05-30 16:32:43 +0300

    user validation

--- controllers/users.controller.ts
+++ controllers/users.controller.ts
@@ -3,6 +3,7 @@ import { RequestExt, ResponseExt } from "../utils/types";
 import { isError, result } from "../utils/error";
 import { userAccountModel } from "../models/userAccount.model";
 import { templateModel } from "../models/template.model";
+import { userValidation } from "../validation/user.validation";
 
 export async function getAllUsers(req: RequestExt, res: ResponseExt) {
 	const user = await result(userAccountModel.find());
@@ -27,12 +28,22 @@ export async function getUserData(req: RequestExt, res: ResponseExt) {
 }
 
 export async function updateUserData(req: RequestExt, res: ResponseExt) {
+
+	const validationResult=userValidation(req.body)
+
+		if (!validationResult.success)
+			return res
+				.status(400)
+				.json({ status: "Error", message: validationResult.error });
+
+		const validateData = validationResult.data.data;
+
 	const userId = req.session.uid;
 	const user = await result(
 		userAccountModel.findOneAndUpdate(
 			{ uid: userId },
 			{
-				$set: { data: req.body.data },
+				$set: { data: validateData },
 			},
 		),
 	);
