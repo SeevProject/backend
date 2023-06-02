8e787b533863089b2ce6e413923049b5ceddfc77
Ahmed K. A <ahmadkabdullah@pm.me>
2023-06-02 10:48:34 +0300

    Add run file to example

--- generator/example/run.ts
+++ generator/example/run.ts
@@ -1,28 +1,28 @@
 import { load } from "cheerio";
 import { readFileSync } from "fs";
-import { convertHTMLtoPDF } from "./converter";
-import { firebaseStorage } from "../utils/firebase";
-import { insertData } from "./inserter";
-import { flattenObject, getCompatability, parseTemplate } from "./parser";
+import { convertHTMLtoPDF } from "../converter";
+import { firebaseStorage } from "../../utils/firebase";
+import { insertData } from "../inserter";
+import { flattenObject, getCompatability, parseTemplate } from "../parser";
+import { userData } from "./data";
 
 export async function generateCV(
 	data: any,
 	templatePath: string,
-	downloadPath: string,
-	outputPath: string,
+	// outputPath: string,
 ) {
-	firebaseStorage.file(templatePath).download({
-		destination: downloadPath,
-	});
 	// init
 
-	const fileString = readFileSync(downloadPath).toString();
+	const fileString = readFileSync(templatePath).toString();
 
 	const document = load(fileString);
 
 	// parser
 
 	let template = parseTemplate(document, templatePath);
+
+	console.log(template);
+
 	let flattenedData = flattenObject(data);
 
 	let problems = getCompatability(flattenedData, template);
@@ -34,6 +34,6 @@ export async function generateCV(
 	insertData(document, flattenObject(data));
 
 	// converter
-
-	await convertHTMLtoPDF(document.html(), outputPath);
 }
+
+generateCV(userData, __dirname + "/cv.html");
