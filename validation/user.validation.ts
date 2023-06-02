3739122caa515ec429defc63fea916d46e27cea4
Ahmed K. A <ahmadkabdullah@pm.me>
2023-06-02 12:31:39 +0300

    Fix users validation for changes

--- validation/user.validation.ts
+++ validation/user.validation.ts
@@ -1,70 +1,92 @@
-import {z} from "zod";
+import { z } from "zod";
 
-export const userValidation=(body)=>{
-
+export const userValidation = (body: any) => {
 	const validation = z
 		.object({
-			data: z.object({
-				name: z.object({
+			name: z
+				.object({
 					first: z.string().optional(),
 					middle: z.string().optional(),
 					last: z.string().optional(),
-				}).optional(),
-				phone: z.string().optional(),
-				email: z.string().optional(),
-				picture: z.string().optional(),
-				about: z.string().optional(),
-				status: z.object({
+				})
+				.optional(),
+			phone: z.string().optional(),
+			email: z.string().optional(),
+			about: z.string().optional(),
+			status: z
+				.object({
 					employed: z.boolean().optional(),
 					student: z.boolean().optional(),
-				}).optional(),
-				address: z.object({
+				})
+				.optional(),
+			address: z
+				.object({
 					country: z.string().optional(),
 					city: z.string().optional(),
 					street: z.string().optional(),
-				}).optional(),
-				languages: z.array(
-					z.object({
-						name: z.string().optional(),
-						level: z.string().optional(),
-					}).optional(),
-				).optional(),
-				education: z.array(
-					z.object({
-						facility: z.string().optional(),
-						location: z.string().optional(),
-						degree_level: z.string().optional(),
-						degree_field: z.string().optional(),
-						date_start: z.string().optional(),
-						date_end: z.string().optional(),
-					}).optional(),
-				).optional(),
-				courses: z.array(
-					z.object({
-						title: z.string().optional(),
-						about: z.string().optional(),
-						subjects: z.string().optional(),
-						location: z.string().optional(),
-					}).optional(),
-				).optional(),
-				skills: z.array(
-					z.object({
-						name: z.string().optional(),
-						about: z.string().optional(),
-						level: z.string().optional(),
-					}).optional(),
-				).optional(),
-				hobbies: z.array(
-					z.object({
-						name: z.string().optional(),
-						about: z.string().optional(),
-					}).optional(),
-				).optional(),
-			}),
-		}).required();
+				})
+				.optional(),
+			languages: z
+				.array(
+					z
+						.object({
+							name: z.string().optional(),
+							level: z.string().optional(),
+						})
+						.optional(),
+				)
+				.optional(),
+			education: z
+				.array(
+					z
+						.object({
+							facility: z.string().optional(),
+							location: z.string().optional(),
+							degree_level: z.string().optional(),
+							degree_field: z.string().optional(),
+							date_start: z.string().optional(),
+							date_end: z.string().optional(),
+						})
+						.optional(),
+				)
+				.optional(),
+			courses: z
+				.array(
+					z
+						.object({
+							title: z.string().optional(),
+							about: z.string().optional(),
+							subjects: z.array(z.string()).optional(),
+							location: z.string().optional(),
+						})
+						.optional(),
+				)
+				.optional(),
+			skills: z
+				.array(
+					z
+						.object({
+							name: z.string().optional(),
+							about: z.string().optional(),
+							level: z.number().optional(),
+						})
+						.optional(),
+				)
+				.optional(),
+			hobbies: z
+				.array(
+					z
+						.object({
+							name: z.string().optional(),
+							about: z.string().optional(),
+						})
+						.optional(),
+				)
+				.optional(),
+		})
+		.required();
 
-	const result =validation.safeParse(body)
+	const result = validation.safeParse(body);
 
-	return result
-}
-z.object({});
+	return result;
+};
