1c6300b37bb5283ee21492724ec9efb7737b9308
Ahmed K. A <ahmadkabdullah@pm.me>
2023-06-05 22:34:14 +0300

    Change cookie settings for https and secure

--- main.ts
+++ main.ts
@@ -59,10 +59,10 @@ app.use(
 			dbName: "testing",
 		}),
 		cookie: {
-			httpOnly: true,
+			httpOnly: false,
 			sameSite: "lax",
 			maxAge: 1000 * 60 * 60 * 24 * 3,
-			secure: env.DEV ? false : true,
+			secure: true,
 		},
 	}),
 
