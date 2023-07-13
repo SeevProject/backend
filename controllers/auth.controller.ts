d0adac868d3ab69cf63df92d0df1362035cec791
lanya yousif <lanyayusf541@gmail.com>
2023-07-13 13:17:42 +0300

    manage response on  auth

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -1,9 +1,10 @@
-import { isError, result } from "../utils/error";
-import { userAccountModel } from "../models/userAccount.model";
-import { RequestExt, ResponseExt } from "../utils/types";
-import { accountModel } from "../models/account.model";
-import { validateRegister } from "../validation/auth.validation";
-import { companyAccountModel } from "../models/companyAccount.model";
+import { isError, result } from '../utils/error';
+import { userAccountModel } from '../models/userAccount.model';
+import { RequestExt, ResponseExt } from '../utils/types';
+import { accountModel } from '../models/account.model';
+import { validateRegister } from '../validation/auth.validation';
+import { companyAccountModel } from '../models/companyAccount.model';
+import { failResponse, successResponse } from '../utils/response';
 
 export async function login(req: RequestExt, res: ResponseExt) {
 	// token data is already added to the request by the previous middleware
@@ -20,20 +21,16 @@ export async function login(req: RequestExt, res: ResponseExt) {
 
 	// if got error while finding account, return error
 	if (isError(account))
-		return res
-			.status(401)
-			.json({ message: "Could not finish search for your account" });
+		return failResponse(res, 401, 'Could not finish search for your account');
 
 	// if user account does not exist, return error
 	if (!account)
-		return res
-			.status(401)
-			.json({ message: "You do not have an account to login to" });
+		return failResponse(res, 401, 'You do not have an account to login to');
 
 	// finally create session using uid retrieved from database
 	req.session.uid = account.uid;
 
-	return res.status(200).json({ message: "successfully logged in" });
+	return successResponse(res, 200, 'successfully logged in', account);
 }
 
 export async function register(req: RequestExt, res: ResponseExt) {
@@ -43,7 +40,7 @@ export async function register(req: RequestExt, res: ResponseExt) {
 	// validate req.body data and return error if not matching
 	const validationResult = validateRegister.safeParse(req.body);
 	if (!validationResult.success)
-		return res.status(400).json({ message: validationResult.error });
+		return failResponse(res, 401, `${validationResult.error}`);
 
 	// use validated body data instead of req.body
 	const validBody = validationResult.data;
@@ -57,15 +54,19 @@ export async function register(req: RequestExt, res: ResponseExt) {
 
 	// if got error while finding account, return error
 	if (isError(account))
-		return res
-			.status(401)
-			.json({ message: "Could not try to find if you already got an account" });
+		return failResponse(
+			res,
+			401,
+			'Could not try to find if you already got an account',
+		);
 
 	// if user account already exists, return error
 	if (account)
-		return res
-			.status(401)
-			.json({ message: "You already got an account, cannot make another" });
+		return failResponse(
+			res,
+			401,
+			'You already got an account, cannot make another',
+		);
 
 	// // create user account in database
 	// const createdAccount = await result(
@@ -80,12 +81,12 @@ export async function register(req: RequestExt, res: ResponseExt) {
 
 	let createdAccount;
 
-	if (validBody.type == "company") {
+	if (validBody.type == 'company') {
 		createdAccount = companyAccountModel.create({
 			username: validBody.username,
 			uid: tokenData?.uid,
 			createdAt: new Date().toString(),
-			type: "company",
+			type: 'company',
 			approved: false,
 		});
 	} else {
@@ -93,27 +94,28 @@ export async function register(req: RequestExt, res: ResponseExt) {
 			username: validBody.username,
 			uid: tokenData?.uid,
 			createdAt: new Date().toString(),
-			type: "user",
+			type: 'user',
 			cvlist: [],
 		});
 	}
 
 	// if could not create account, return error
 	if (isError(createdAccount))
-		return res
-			.status(401)
-			.json({ message: "Could not create your account in db" });
+		return failResponse(res, 401, 'Could not create your account in db');
 
 	// finally create session using uid retrieved from database
 	req.session.uid = createdAccount.uid;
-
-	return res.send("successfully registered the account");
+	return successResponse(
+		res,
+		200,
+		'successfully registered the account',
+		createdAccount,
+	);
 }
 
 export async function loginOrRegister(req: RequestExt, res: ResponseExt) {
 	// token data is already added to the request by the previous middleware
 	const tokenData = req.tokenData;
-
 	//try to find user account in database
 	const account = await result(
 		accountModel.findOne({
@@ -123,20 +125,19 @@ export async function loginOrRegister(req: RequestExt, res: ResponseExt) {
 
 	// if got error while finding account, return error
 	if (isError(account))
-		return res
-			.status(401)
-			.json({ message: 'Could not try to find if you already got an account' });
+		return failResponse(
+			res,
+			401,
+			'Could not try to find if you already got an account',
+		);
 
 	// if the account exists set session and return
 	if (account) {
 		req.session.uid = account.uid;
-		return res.status(200).json({ message: 'successfully logged in' });
+		return successResponse(res, 200, 'successfully logged in', account);
 	}
 
 	// else register the account
-
-
-	console.log(req.body)
 	// validate req.body data and return error if not matching
 	const validationResult = validateRegister.safeParse(req.body);
 	if (!validationResult.success)
@@ -168,20 +169,28 @@ export async function loginOrRegister(req: RequestExt, res: ResponseExt) {
 
 	// if could not create account, return error
 	if (isError(createdAccount))
-		return res
-			.status(401)
-			.json({ message: 'Could not create your account in db' });
+		return failResponse(res, 401, 'Could not create your account in db');
 
 	// finally create session using uid retrieved from database
 	req.session.uid = createdAccount.uid; //createdAccount.uid;
 
-	return res.send('successfully registered the account');
+	return successResponse(
+		res,
+		200,
+		'successfully registered the account',
+		createdAccount,
+	);
 }
 
 export async function logout(req: RequestExt, res: ResponseExt) {
 	// delete session for user
 	req.session.destroy((err) => {
-		if (err) return res.status(500).json({ message: "error logging out" });
+		if (err) return failResponse(res, 500, 'error logging out');
 	});
-	return res.status(200).json({ message: "successfully logged user out" });
+	return successResponse(
+		res,
+		200,
+		'successfully logged user out',
+		'not user exit',
+	);
 }
